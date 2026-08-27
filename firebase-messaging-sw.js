0608446e019b84ed4e2b97d8821d9ddc5cdfaf4f
inventariosarda <giulianova2025@gmail.com>
2026-08-27 23:35:44 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,21 +2,23 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyB0daUTnfyhdyWPpkmFoPcyAguyZq63NE0",[cite: 1]
-    authDomain: "gestionepedane.firebaseapp.com",[cite: 1]
-    projectId: "gestionepedane",[cite: 1]
-    storageBucket: "gestionepedane.firebasestorage.app",[cite: 1]
-    messagingSenderId: "865581696674",[cite: 1]
-    appId: "1:865581696674:web:876ac3f6c5b34e1ccd39fb"[cite: 1]
+    apiKey: "AIzaSyB0daUTnfyhdyWPpkmFoPcyAguyZq63NE0",
+    authDomain: "gestionepedane.firebaseapp.com",
+    projectId: "gestionepedane",
+    storageBucket: "gestionepedane.firebasestorage.app",
+    messagingSenderId: "865581696674",
+    appId: "1:865581696674:web:876ac3f6c5b34e1ccd39fb"
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-    const title = payload.notification.title;
+    console.log('[firebase-messaging-sw.js] Notifica in background:', payload);
+    
+    const title = payload.notification?.title || 'Nuova Notifica';
     const options = {
-        body: payload.notification.body,
-        icon: '/icon.png',
+        body: payload.notification?.body || '',
+        icon: '/gestione-cella/icon.png',
         vibrate: [200, 100, 200],
         data: payload.data
     };
